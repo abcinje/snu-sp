4d136418320ca80fa9759b5b88a4e8082fa7dce4
abcinje <abcinje@gmail.com>
2019-03-13 21:29:46 +0900

    Implement part2

--- linklab/part2/memtrace.c
+++ linklab/part2/memtrace.c
@@ -80,6 +80,16 @@ void fini(void)
 	LOG_STATISTICS(n_allocb,
 		n_allocb / (n_malloc+n_calloc+n_realloc), n_freeb);
 
+	LOG_NONFREED_START();
+	if (list != NULL) {
+		item *i = list->next;
+		while (i != NULL) {
+			if (i->cnt)
+				LOG_BLOCK(i->ptr, i->size, i->cnt, i->fname, i->ofs);
+			i = i->next;
+		}
+	}
+
 	LOG_STOP();
 
 	free_list(list);
