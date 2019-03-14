bcc18adf27cb1c9a4d79804cb90c93ed6c61b54c
abcinje <abcinje@gmail.com>
2019-03-15 00:35:44 +0900

    Solve part3 except for some cases

--- linklab/part3/callinfo.c
+++ linklab/part3/callinfo.c
@@ -1,6 +1,34 @@
 #include <stdlib.h>
+#include <string.h>
+
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
 
 int get_callinfo(char *fname, size_t fnlen, unsigned long long *ofs)
 {
-  return -1;
+	unw_context_t ctxt;
+	unw_cursor_t c;
+
+	if (unw_getcontext(&ctxt) || unw_init_local(&c, &ctxt))
+		return -1;
+
+	while (1) {
+		unw_step(&c);
+		unw_get_proc_name(&c, fname, fnlen, (unw_word_t *)ofs);
+
+		if (!strlen(fname))
+			return -1;
+
+		if (!strcmp(fname, "malloc")	||
+		    !strcmp(fname, "free")	||
+		    !strcmp(fname, "calloc")	||
+		    !strcmp(fname, "realloc"))
+			break;
+	}
+
+	unw_step(&c);
+	unw_get_proc_name(&c, fname, fnlen, (unw_word_t *)ofs);
+	*ofs -= 5;
+
+	return 0;
 }
