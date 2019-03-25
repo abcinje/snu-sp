91529b9bbb7fc876666a936ce69746183f0fe206
abcinje <abcinje@gmail.com>
2019-03-25 14:54:10 +0900

    Invoke realloc error in part2

--- linklab/part2/memtrace.c
+++ linklab/part2/memtrace.c
@@ -140,8 +140,6 @@ void *realloc(void *ptr, size_t size)
 	item *i = find(list, ptr);
 	if (i && i->cnt)
 		n_freeb += i->size;
-	else
-		ptr = NULL;
 
 	char *new_ptr = reallocp(ptr, size);
 	dealloc(list, ptr);
