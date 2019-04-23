440bb7203e9ef8d20456f056d03c589579b80a48
abcinje <abcinje@gmail.com>
2019-04-24 04:17:05 +0900

    Implement ptree module

--- kernellab/ptree/dbfs_ptree.c
+++ kernellab/ptree/dbfs_ptree.c
@@ -1,61 +1,94 @@
 #include <linux/debugfs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 #include <linux/uaccess.h>
+
+#define BUF_SIZE	1024
+#define INFO_SIZE	256
 
 MODULE_LICENSE("GPL");
 
-static struct dentry *dir, *inputdir, *ptreedir;
+static struct dentry *dir, *input, *ptree;
 static struct task_struct *curr;
+static u32 pid;
 
-static ssize_t write_pid_to_input(struct file *fp, 
-                                const char __user *user_buffer, 
-                                size_t length, 
-                                loff_t *position)
+static ssize_t ptree_read(struct file *fp, char __user *user_buf,
+				size_t count, loff_t *pos)
 {
-        pid_t input_pid;
+	struct task_struct *task;
+	char *buf, *proc_info;
+	int cursor, len;
+	ssize_t ret;
 
-        //sscanf(user_buffer, "%u", &input_pid);
-        //curr = // Find task_struct using input_pid. Hint: pid_task
+	buf = kzalloc(BUF_SIZE, GFP_KERNEL);
+	proc_info = kzalloc(INFO_SIZE, GFP_KERNEL);
 
-        // Tracing process tree from input_pid to init(1) process
+	if (!(task = pid_task(find_get_pid(pid), PIDTYPE_PID))) {
+		strncpy(buf, "No such process\n", BUF_SIZE);
+		ret = simple_read_from_buffer(user_buf, count, pos, buf, strlen(buf));
+	}
+	
 
-        // Make Output Format string: process_command (process_id)
+	else {
+		cursor = BUF_SIZE - 1;
 
-        return length;
+		while (task->pid) {
+			snprintf(proc_info, INFO_SIZE, "%s (%d)\n", task->comm, task->pid);
+			len = strlen(proc_info);
+			cursor -= len;
+			strncpy(buf + cursor, proc_info, len);
+			task = task->parent;
+		}
+
+		ret = simple_read_from_buffer(user_buf, count, pos, buf + cursor, strlen(buf + cursor));
+	}
+
+	kfree(buf);
+	kfree(proc_info);
+
+	return ret;
 }
 
-static const struct file_operations dbfs_fops = {
-        .write = write_pid_to_input,
+static const struct file_operations ptree_fops = {
+	.owner = THIS_MODULE,
+	.read = ptree_read
 };
 
 static int __init dbfs_module_init(void)
 {
-        // Implement init module code
+	if (!(dir = debugfs_create_dir("ptree", NULL))) {
+		printk("Cannot create ptree dir\n");
+		goto fail;
+	}
 
-#if 0
-        dir = debugfs_create_dir("ptree", NULL);
-        
-        if (!dir) {
-                printk("Cannot create ptree dir\n");
-                return -1;
-        }
+	if (!(input = debugfs_create_u32("input", 0644, dir, &pid))) {
+		printk("Cannot create input file\n");
+		goto fail;
+	}
 
-        inputdir = debugfs_create_file("input", , , , );
-        ptreedir = debugfs_create_("ptree", , , ); // Find suitable debugfs API
-#endif
+	if (!(ptree = debugfs_create_file("ptree", 0444, dir, NULL, &ptree_fops))) {
+		printk("Cannot create ptree file\n");
+		goto fail;
+	}
 	
 	printk("dbfs_ptree module initialize done\n");
-
         return 0;
+
+fail:
+	if (dir)
+		debugfs_remove_recursive(dir);
+
+	return -1;
 }
 
 static void __exit dbfs_module_exit(void)
 {
-        // Implement exit module code
+	debugfs_remove_recursive(dir);
 	
 	printk("dbfs_ptree module exit\n");
 }
 
 module_init(dbfs_module_init);
 module_exit(dbfs_module_exit);
+
