f5fdffe1a5236dd8f23e55638614f760c92fde63
abcinje <abcinje@gmail.com>
2019-05-02 01:34:23 +0900

    Implement paddr module

--- kernellab/paddr/dbfs_paddr.c
+++ kernellab/paddr/dbfs_paddr.c
@@ -6,48 +6,81 @@
 
 MODULE_LICENSE("GPL");
 
+struct packet {
+	pid_t pid;
+	unsigned long vaddr;
+	unsigned long paddr;
+};
+
 static struct dentry *dir, *output;
-static struct task_struct *task;
 
 static ssize_t read_output(struct file *fp,
-                        char __user *user_buffer,
-                        size_t length,
-                        loff_t *position)
+			char __user *user_buf,
+			size_t count,
+			loff_t *pos)
 {
-        // Implement read file operation
+	struct packet pckt;
+	struct task_struct *task;
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *pte;
+
+	memcpy(&pckt, user_buf, sizeof(pckt));
+
+	if (!(task = pid_task(find_get_pid(pckt.pid), PIDTYPE_PID)))
+		return -ESRCH;
+
+	pgd = pgd_offset(task->mm, pckt.vaddr);
+	if (pgd_none(*pgd))
+		return -EFAULT;
+
+	pud = pud_offset(pgd, pckt.vaddr);
+	if (pud_none(*pud))
+		return -EFAULT;
+
+	pmd = pmd_offset(pud, pckt.vaddr);
+	if (pmd_none(*pmd))
+		return -EFAULT;
+
+	pte = pte_offset_kernel(pmd, pckt.vaddr);
+	if (pte_none(*pte))
+		return -EFAULT;
+
+	pckt.paddr = (pte_val(*pte) & PTE_PFN_MASK) | (pckt.vaddr & ~PAGE_MASK);
+
+	return simple_read_from_buffer(user_buf, count, pos, &pckt, sizeof(pckt));
 }
 
-static const struct file_operations dbfs_fops = {
-        // Mapping file operations with your functions
+static const struct file_operations output_fops = {
+	.owner = THIS_MODULE,
+	.read = read_output
 };
 
 static int __init dbfs_module_init(void)
 {
-        // Implement init module
+	if (!(dir = debugfs_create_dir("paddr", NULL))) {
+		printk("Cannot create paddr dir\n");
+		return -1;
+	}
 
-#if 0
-        dir = debugfs_create_dir("paddr", NULL);
-
-        if (!dir) {
-                printk("Cannot create paddr dir\n");
-                return -1;
-        }
-
-        // Fill in the arguments below
-        output = debugfs_create_file("output", , , , );
-#endif
+	if (!(output = debugfs_create_file("output", 0444, dir, NULL, &output_fops))) {
+		printk("Cannot create output file\n");
+		debugfs_remove(dir);
+		return -1;
+	}
 
 	printk("dbfs_paddr module initialize done\n");
-
-        return 0;
+	return 0;
 }
 
 static void __exit dbfs_module_exit(void)
 {
-        // Implement exit module
+	debugfs_remove_recursive(dir);
 
 	printk("dbfs_paddr module exit\n");
 }
 
 module_init(dbfs_module_init);
 module_exit(dbfs_module_exit);
+
