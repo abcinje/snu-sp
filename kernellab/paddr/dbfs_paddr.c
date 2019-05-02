483139b01a5468b39149efbf35cc260a0a00af7b
abcinje <abcinje@gmail.com>
2019-05-02 23:52:44 +0900

    Add comments

--- kernellab/paddr/dbfs_paddr.c
+++ kernellab/paddr/dbfs_paddr.c
@@ -4,8 +4,6 @@
 #include <linux/uaccess.h>
 #include <asm/pgtable.h>
 
-MODULE_LICENSE("GPL");
-
 struct packet {
 	pid_t pid;
 	unsigned long vaddr;
@@ -13,6 +11,10 @@ struct packet {
 };
 
 static struct dentry *dir, *output;
+
+/*************************
+ * output read operation
+ *************************/
 
 static ssize_t read_output(struct file *fp,
 			char __user *user_buf,
@@ -26,44 +28,61 @@ static ssize_t read_output(struct file *fp,
 	pmd_t *pmd;
 	pte_t *pte;
 
+	/* Copy the contents of user_buf to pckt */
 	memcpy(&pckt, user_buf, sizeof(pckt));
 
+	/* If a task doesn't exist for the given pid, return error */
 	if (!(task = pid_task(find_get_pid(pckt.pid), PIDTYPE_PID)))
 		return -ESRCH;
 
+	/* Find the pgd entry from the mm_struct structure */
 	pgd = pgd_offset(task->mm, pckt.vaddr);
 	if (pgd_none(*pgd))
 		return -EFAULT;
 
+	/* Find the pud entry from the pgd entry */
 	pud = pud_offset(pgd, pckt.vaddr);
 	if (pud_none(*pud))
 		return -EFAULT;
 
+	/* Find the pmd entry from the pud entry */
 	pmd = pmd_offset(pud, pckt.vaddr);
 	if (pmd_none(*pmd))
 		return -EFAULT;
 
+	/* Find the pte entry from the pmd entry */
 	pte = pte_offset_kernel(pmd, pckt.vaddr);
 	if (pte_none(*pte))
 		return -EFAULT;
 
+	/* Compute the physical address */
 	pckt.paddr = (pte_val(*pte) & PTE_PFN_MASK) | (pckt.vaddr & ~PAGE_MASK);
 
 	return simple_read_from_buffer(user_buf, count, pos, &pckt, sizeof(pckt));
 }
+
+/***********************************
+ * output file_operations structure
+ ***********************************/
 
 static const struct file_operations output_fops = {
 	.owner = THIS_MODULE,
 	.read = read_output
 };
 
+/*************************
+ * module init and exit
+ *************************/
+
 static int __init dbfs_module_init(void)
 {
+	/* Create paddr directory */
 	if (!(dir = debugfs_create_dir("paddr", NULL))) {
 		printk("Cannot create paddr dir\n");
 		return -1;
 	}
 
+	/* Create output file */
 	if (!(output = debugfs_create_file("output", 0444, dir, NULL, &output_fops))) {
 		printk("Cannot create output file\n");
 		debugfs_remove(dir);
@@ -76,6 +95,7 @@ static int __init dbfs_module_init(void)
 
 static void __exit dbfs_module_exit(void)
 {
+	/* Remove paddr directory recursively */
 	debugfs_remove_recursive(dir);
 
 	printk("dbfs_paddr module exit\n");
@@ -83,4 +103,8 @@ static void __exit dbfs_module_exit(void)
 
 module_init(dbfs_module_init);
 module_exit(dbfs_module_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kang Injae");
+MODULE_DESCRIPTION("Finding Physical Address");
 
--- kernellab/ptree/dbfs_ptree.c
+++ kernellab/ptree/dbfs_ptree.c
@@ -7,10 +7,12 @@
 #define BUF_SIZE	1024
 #define INFO_SIZE	256
 
-MODULE_LICENSE("GPL");
-
 static struct dentry *dir, *input, *ptree;
 static u32 pid;
+
+/*************************
+ * ptree read operation
+ *************************/
 
 static ssize_t read_ptree(struct file *fp, char __user *user_buf,
 				size_t count, loff_t *pos)
@@ -20,17 +22,22 @@ static ssize_t read_ptree(struct file *fp, char __user *user_buf,
 	int cursor, len;
 	ssize_t ret;
 
+	/* Allocate buf and proc_info buffers */
 	buf = kzalloc(BUF_SIZE, GFP_KERNEL);
 	proc_info = kzalloc(INFO_SIZE, GFP_KERNEL);
 
+	/* If a task doesn't exist for the given pid, pass a message */
 	if (!(task = pid_task(find_get_pid(pid), PIDTYPE_PID))) {
 		strncpy(buf, "No such process\n", BUF_SIZE);
 		ret = simple_read_from_buffer(user_buf, count, pos, buf, strlen(buf));
 	}
 
+	/* If a task exists, trace the process tree */
 	else {
 		cursor = BUF_SIZE - 1;
 
+		/* Store process name and id for each process in buf
+		   from the back of the buffer to the front */
 		while (task->pid) {
 			snprintf(proc_info, INFO_SIZE, "%s (%d)\n", task->comm, task->pid);
 			len = strlen(proc_info);
@@ -42,29 +49,41 @@ static ssize_t read_ptree(struct file *fp, char __user *user_buf,
 		ret = simple_read_from_buffer(user_buf, count, pos, buf + cursor, strlen(buf + cursor));
 	}
 
+	/* Deallocate buf and proc_info buffers */
 	kfree(buf);
 	kfree(proc_info);
 
 	return ret;
 }
 
+/***********************************
+ * ptree file_operations structure
+ ***********************************/
+
 static const struct file_operations ptree_fops = {
 	.owner = THIS_MODULE,
 	.read = read_ptree
 };
 
+/*************************
+ * module init and exit
+ *************************/
+
 static int __init dbfs_module_init(void)
 {
+	/* Create ptree directory */
 	if (!(dir = debugfs_create_dir("ptree", NULL))) {
 		printk("Cannot create ptree dir\n");
 		goto fail;
 	}
 
+	/* Create input file */
 	if (!(input = debugfs_create_u32("input", 0644, dir, &pid))) {
 		printk("Cannot create input file\n");
 		goto fail;
 	}
 
+	/* Create ptree file */
 	if (!(ptree = debugfs_create_file("ptree", 0444, dir, NULL, &ptree_fops))) {
 		printk("Cannot create ptree file\n");
 		goto fail;
@@ -74,6 +93,7 @@ static int __init dbfs_module_init(void)
         return 0;
 
 fail:
+	/* If directory has been created, remove it recursively */
 	if (dir)
 		debugfs_remove_recursive(dir);
 
@@ -82,6 +102,7 @@ fail:
 
 static void __exit dbfs_module_exit(void)
 {
+	/* Remove ptree directory recursively */
 	debugfs_remove_recursive(dir);
 	
 	printk("dbfs_ptree module exit\n");
@@ -89,4 +110,8 @@ static void __exit dbfs_module_exit(void)
 
 module_init(dbfs_module_init);
 module_exit(dbfs_module_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kang Injae");
+MODULE_DESCRIPTION("Process Tree Tracing");
 
